a8e3421a3a344566d2ad92adc3b7ee7dc7a3dae0
hanbin <hanbin6157@gmail.com>
2025-05-13 15:33:12 +0200

    Devlop basic internal installation process

---
+++ OTA_Handler/send_list/handler_tcp_client/build/CMakeFiles/handler_tcp_client-someip.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/handler_tcp_client-someip.dir/home/ota/Documents/src-gen/someip/v0/commonapi/Handler_msgSomeIPDeployment.cpp.o"
+  "CMakeFiles/handler_tcp_client-someip.dir/home/ota/Documents/src-gen/someip/v0/commonapi/Handler_msgSomeIPDeployment.cpp.o.d"
+  "CMakeFiles/handler_tcp_client-someip.dir/home/ota/Documents/src-gen/someip/v0/commonapi/Handler_msgSomeIPProxy.cpp.o"
+  "CMakeFiles/handler_tcp_client-someip.dir/home/ota/Documents/src-gen/someip/v0/commonapi/Handler_msgSomeIPProxy.cpp.o.d"
+  "CMakeFiles/handler_tcp_client-someip.dir/home/ota/Documents/src-gen/someip/v0/commonapi/Handler_msgSomeIPStubAdapter.cpp.o"
+  "CMakeFiles/handler_tcp_client-someip.dir/home/ota/Documents/src-gen/someip/v0/commonapi/Handler_msgSomeIPStubAdapter.cpp.o.d"
+  "libhandler_tcp_client-someip.pdb"
+  "libhandler_tcp_client-someip.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/handler_tcp_client-someip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
